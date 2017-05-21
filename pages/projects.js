a1bb0dea897aab60186c9c25c70eb3f9b7dc1057
LasaleFamine <info@godev.space>
2017-05-21 12:06:11 +0200

    other solution for analytics

--- pages/projects.js
+++ pages/projects.js
@@ -5,23 +5,11 @@ import Page from './../layouts/main'
 import Projects from './../components/project-boxes'
 import Footer from './../components/footer'
 
-const analytics = () => {
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-74988372-1', 'auto');
-	ga('send', 'pageview');
-}
-
 export default () => (
 	<Page>
 		<Head>
 			<title>Godev.Space/projects</title>
-			<script>
-				analytics()
-			</script>
+			<script src="/static/analytics.js" />
 		</Head>
 
 		<section className="hero is-black">
