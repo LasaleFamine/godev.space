20d213f186aa710496438331b9dbb67197e3ab76
LasaleFamine <info@godev.space>
2017-06-21 19:43:11 +0200

    Added favicon and icons

--- components/meta.js
+++ components/meta.js
@@ -8,7 +8,13 @@ export default () => (
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 			<link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css" rel="stylesheet" type="text/css" />
 			<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet" type="text/css" />
-    </Head>
+			<link rel="apple-touch-icon" sizes="180x180" href="/static/icons/apple-touch-icon.png" />
+			<link rel="icon" type="image/png" sizes="32x32" href="/static/icons/favicon-32x32.png" />
+			<link rel="icon" type="image/png" sizes="16x16" href="/static/icons/favicon-16x16.png" />
+			<link rel="manifest" href="/static/manifest.json" />
+			<link rel="mask-icon" href="/static/icons/safari-pinned-tab.svg" color="#5bbad5" />
+			<meta name="theme-color" content="#ffffff" />
+	</Head>
 
     { /* global styles */ }
     <style jsx global>{`
